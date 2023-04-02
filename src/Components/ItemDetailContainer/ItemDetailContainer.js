bc862164fb95b14057d5109bee62555de19d79cb
Adrian Muñoz <adrianmunoz2005@gmail.com>
2023-04-02 20:26:04 -0300

    Firebase

--- src/Components/ItemDetailContainer/ItemDetailContainer.js
+++ src/Components/ItemDetailContainer/ItemDetailContainer.js
@@ -1,26 +1,35 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
-import { pedirDatos } from "../../helpers/pedirDatos"
 import ItemDetail from "../ItemDetail/ItemDetail"
 import './ItemDetailContainer.scss'
 import { Link } from "react-router-dom"
 import { Loader } from "../Loader/Loader"
-
+import { doc, getDoc } from "firebase/firestore"
+import { db } from "../../Firebase/config"
 
 export const ItemDetailContainer = () => {
     const [item, setItem] = useState(null)
     const [loading, setLoading] = useState(true)
-
+   
     const { itemId } = useParams()
 
    
     useEffect(() => {
         setLoading(true)
+        //Firebase
+        //1-armo la ref (syn)
+        const docRef = doc(db, "productos", itemId)
 
-        pedirDatos()
-            .then((res) => {
-                setItem( res.find((prod) => prod.id === Number(itemId)) )
+        //2-llamo a la ref(asyn)
+        getDoc(docRef)
+            .then((doc) => {
+                setItem({
+                    id: doc.id,
+                    ...doc.data()
+                })
+               
             })
+
             .finally(() => {
                 setLoading(false)
             })
--- src/Components/ItemListContainer/ItemListContainer.js
+++ src/Components/ItemListContainer/ItemListContainer.js
@@ -1,45 +1,44 @@
 
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { pedirDatos } from '../../helpers/pedirDatos';
 import  { ItemList } from '../ItemList/ItemList'
 import './ItemListContainer.scss'
-import ReactLoading from "react-loading";
-
+import { Loader } from "../Loader/Loader"
+import { collection, getDocs, query, where } from 'firebase/firestore';
+import { db } from '../../Firebase/config';
 
 export const ItemListContainer = () => {
 
-//(4)una vez creada la promesa y mostrar la accion de la misma, necesitamos 
-//la estructura para renderizar y guardar ya que es asincronico
-//productos, inicia con un array vacio
 const [productos, setProductos] = useState([])
 const [loading, setLoading] = useState(true)
- 
+
 // parametros de navegacion
 const { categoryId} = useParams()
 
 
-//(2) y (3) efecto de montaje para que renderizemos el resultado de la promesa una vez que 
-//se halla resuelto y en base a esa respuesta pedimos datos
 useEffect(()=>{
    setLoading(true)
-     // llamamos a la funcion pedir datos que ejecuta una promesa
-     // para definir que accion tomar en caso se resuelva o se rechaze la promesa
-    pedirDatos()
-        .then((res)=>{
-            // filtra  y muestra los productos por categoria de MOCK_DATA
-            if (categoryId) {
-                setProductos( res.filter((prod)=> prod.category === categoryId))
-            } else {
-         //res : resuelve como el array MOCK_DATA y se lo envia a la variable: setProducto, que luego actualiza Productos
-    
-                setProductos(res)
-            }
-           
-        })
-        .catch((rej)=>{
-            console.log(rej)
-        })
+
+            //Firebase
+         // 1.- armar una referencia (sync)
+         const productosRef = collection(db, "productos")
+        //filtrado a base de datos para categorias
+        const q = categoryId 
+        ? query(productosRef, where("category", "==", categoryId))
+        : productosRef      
+        // 2.- llamar a esa referencia (async)
+        getDocs(q)
+            .then((res) =>{
+
+                setProductos(res.docs.map((doc)=> { 
+                    return {
+                        id: doc.id,
+                        ...doc.data()
+                    }
+
+                }))
+            })
+
         .finally(() =>{
             setLoading(false)
         })
@@ -52,9 +51,7 @@ return (
             {
                 
                 loading
-                ? (  <div className="spinner-container">
-                <ReactLoading type={"bars"} color={"#123abc"} height={50} width={50} />
-              </div>)
+                ?<Loader/>
                 : <ItemList items={productos}/> 
             }
         
